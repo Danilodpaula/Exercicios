40aa3f189afd1af5b12717df433fba9f3d6f352b
Danilo De Paula <120533663+Danilodpaula@users.noreply.github.com>
2022-12-13 23:49:28 -0300

    Add files via upload

---
+++ Exercicio_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main (void){
+    float area, base, altura;
+    printf("Valor da base: ");
+    scanf("%f", &base);
+    printf("Valor da altura: ");
+    scanf("%f", &altura);
+    area = (base * altura)/2;
+    printf("Valor da area: %.2f. \n", area);
+    system("pause");
+    return 0;
+}
