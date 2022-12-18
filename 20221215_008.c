f86134fa0ebf4368689ef70cfc0fc3497a9896e3
Danilo De Paula <120533663+Danilodpaula@users.noreply.github.com>
2022-12-18 01:47:02 -0400

    Add files via upload

---
+++ 20221215_008.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    float tempf = 0;
+    float tempc = 0;
+
+    printf("Insira a temperatura em Celsius: ");
+    scanf("%f", &tempc);
+
+    tempf = (tempc * 9/5);
+
+    printf("\Tempertura = %.2f°F\n ", tempf);
+
+    return 0;
+}
